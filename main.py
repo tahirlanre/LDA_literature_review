c7a1d738eefaeb3e8481574659914283163e164e
Tahir <tahirlanre@hotmail.com>
2019-06-24 21:51:51 +0100

    Add LDAVis

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from nltk.tokenize import RegexpTokenizer
-from gensim.utils import simple_preprocess
-from gensim.parsing.preprocessing import STOPWORDS
 from nltk.stem.porter import PorterStemmer
-from nltk.stem import WordNetLemmatizer, SnowballStemmer
-from gensim import corpora, models
+from nltk.stem import WordNetLemmatizer
+
 import gensim
+from gensim import corpora, models
+from gensim.utils import simple_preprocess
+
+import pyLDAvis.gensim
+import numpy as np
+
+#SOME_FIXED_SEED = 42
+
+# before training/inference:
+#np.random.seed(SOME_FIXED_SEED)
 
 #Extract abstracts from txt file and put them in a list
 with open('data/abstracts.txt', mode='r', encoding='utf-8-sig') as file:
@@ -30,7 +37,6 @@ processed_docs = []
 
 for abstract in abstract_set:
     processed_docs.append(preprocess(abstract))
-
 #create dictionary from processed docs
 dictionary = corpora.Dictionary(processed_docs)
 
@@ -38,8 +44,11 @@ dictionary = corpora.Dictionary(processed_docs)
 corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
 
 #Run LDA
-lda_model = gensim.models.LdaMulticore(corpus, num_topics=5, id2word = dictionary, passes=10)
+lda_model = gensim.models.LdaMulticore(corpus, num_topics=5, id2word=dictionary, passes=10)
 
 for idx, topic in lda_model.print_topics(-1):
     print("Topic: {} \nWords: {}".format(idx, topic ))
     print("\n")
+    
+LDA_data = pyLDAvis.gensim.prepare(lda_model, corpus, dictionary)
+pyLDAvis.show(LDA_data)
